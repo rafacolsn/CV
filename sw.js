0679f10154cab98b465f37ced1b67f590e779bf1
rafacolsn <raphcolson@gmail.com>
2019-03-28 08:55:59 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bd6da4280e1e5fdb9a5c.js"
+    "url": "webpack-runtime-79a3a0b5276976634551.js"
   },
   {
     "url": "app-ca41a6540897575f7475.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "37052eb8baffdc66cabbe44946edb587"
+    "revision": "b8cab185c2fb8712ce61b07e949a0fff"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
